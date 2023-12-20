96f02297bf4ed370620fb812701055145880ef4b
didvheh <yelee3133@gmail.com>
2023-12-20 13:15:32 +0900

    Test3.java 추가

--- Test1.java
+++ Test1.java
@@ -2,8 +2,5 @@ public class Test1() {
     public static void main(String[] args){
         TestVO vo = new TestVO();
         vo.name = "마루치";
-
-        Test2 t2 = new Test2();
-        t2.price = 200;
     }
 }
