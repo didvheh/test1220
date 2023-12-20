4a948337597cfe619e45c377898d6bb86c1f6390
didvheh <yelee3133@gmail.com>
2023-12-20 10:44:22 +0900

    메인 파일 기능2 추가

--- Test1.java
+++ Test1.java
@@ -2,5 +2,8 @@ public class Test1() {
     public static void main(String[] args){
         TestVO vo = new TestVO();
         vo.name = "마루치";
+
+        Test2 t2 = new Test2();
+        t2.price = 200;
     }
 }
